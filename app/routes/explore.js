145004ea7447a0cf38d31166f72c75828f8d2fa6
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-12-18 21:30:26 +0300

    Explore page

---
+++ app/routes/explore.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const db = require('../services/db');
+var router = express.Router();
+
+/* POST process. */
+router.get('/', async (req, res, next) => {
+    const query = 'SELECT SNP.id, SNP.description, Genotype.magnitude, Genotype.repute, Genotype.summary, Genotype.description, Genotype.allele1, Genotype.allele2 \
+        FROM SNP \
+        JOIN Genotype ON SNP.id = Genotype.snp_id \
+        ORDER BY Genotype.magnitude DESC \
+        LIMIT 100;';
+
+
+    // get the data from the database
+    const results = await db.query(query);
+
+    console.log(results);
+
+    // render the page
+    res.render('explore', { results });
+});
+
+module.exports = router;
--- app/routes/main.js
+++ app/routes/main.js
@@ -1,10 +1,12 @@
 const homeRouter = require('./home');
-const processRouter = require('./process');
+const analyzeRouter = require('./analyze');
+const exploreRouter = require('./explore');
 
 module.exports = (app) => {
     // purpose: display the home page /
     // inputs: no
     // outputs: html generated using template home.ejs
     app.use('/', homeRouter);
-    app.use('/process', processRouter);
+    app.use('/analyze', analyzeRouter);
+    app.use('/explore', exploreRouter);
 };
