4625754dd49d518585859fdc1e22f4e7d88677a1
Dzmitry Plashchynski <plashchynski@gmail.com>
2024-01-04 14:54:08 +0300

    Explore

--- app/routes/explore.js
+++ app/routes/explore.js
@@ -9,21 +9,44 @@ router.get('/', async (req, res, next) => {
     const limit = 10;
     const offset = (page - 1) * limit;
 
-    const query = 'SELECT SNP.id, SNP.description, Genotype.magnitude, Genotype.repute, Genotype.summary, Genotype.description, Genotype.allele1, Genotype.allele2 \
-        FROM SNP \
-        JOIN Genotype ON SNP.id = Genotype.snp_id \
-        ORDER BY Genotype.magnitude DESC LIMIT ? OFFSET ?;';
+    // full text search
+    const search = req.query.search || '';
 
-    // get the data from the database
-    const results = await db.query(query, [limit, offset].map(String));
+    let results;
+    let count_reqults;
 
-    // get the total number of pages
-    const count_query = 'SELECT COUNT(*) FROM SNP JOIN Genotype ON SNP.id = Genotype.snp_id;';
-    const count_reqults = await db.query(count_query);
+    if (search) {
+        const query = 'SELECT SNP.id, SNP.description, Genotype.magnitude, Genotype.repute, Genotype.summary, Genotype.description, Genotype.allele1, Genotype.allele2 \
+            FROM SNP \
+            JOIN Genotype ON SNP.id = Genotype.snp_id \
+            WHERE MATCH (SNP.id, SNP.description) AGAINST (? IN NATURAL LANGUAGE MODE) \
+            ORDER BY Genotype.magnitude DESC LIMIT ? OFFSET ?;';
+
+        // get the total number of pages
+        const count_query = 'SELECT COUNT(*) FROM SNP JOIN Genotype ON SNP.id = Genotype.snp_id \
+                                WHERE MATCH (SNP.id, SNP.description) AGAINST (? IN NATURAL LANGUAGE MODE)';
+        count_reqults = await db.query(count_query, [search]);
+
+        // get the data from the database
+        results = await db.query(query, [search, limit, offset].map(String));
+    } else {
+        const query = 'SELECT SNP.id, SNP.description, Genotype.magnitude, Genotype.repute, Genotype.summary, Genotype.description, Genotype.allele1, Genotype.allele2 \
+            FROM SNP \
+            JOIN Genotype ON SNP.id = Genotype.snp_id \
+            ORDER BY Genotype.magnitude DESC LIMIT ? OFFSET ?;';
+
+        // get the total number of pages
+        const count_query = 'SELECT COUNT(*) FROM SNP JOIN Genotype ON SNP.id = Genotype.snp_id;';
+        count_reqults = await db.query(count_query);
+
+        // get the data from the database
+        results = await db.query(query, [limit, offset].map(String));
+    }
+
     const page_count = Math.ceil(count_reqults[0]['COUNT(*)'] / limit);
 
     // render the page
-    res.render('explore', { results, page, page_count });
+    res.render('explore', { results, page, page_count, search });
 });
 
 module.exports = router;
